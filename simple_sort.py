5321ef867a08c66c8484363cca7e6ae1790b1f43
pipistero <71589375+pipistero@users.noreply.github.com>
2020-09-20 17:47:31 +0300

    Fixed one mistake :(

--- simple_sort.py
+++ simple_sort.py
@@ -2,12 +2,13 @@
 Written 20.09.2020
 Author: Anton Kazakevich
 Algorithm: VERY bad sort :)
-Language: Python 3.x
+Laguage: Python 3.x
 '''
 
 from random import randint
 
-max_int = 100 #Max int that can be generated
+min_int = 0 #Min integer that can be generated
+max_int = 100 #Max intteger that can be generated
 lenght = 10 #Lenght of the array
 arr = []
 sorted_arr = []
@@ -16,8 +17,12 @@ rand_int = randint(0, max_int)
 #Filling the array with random integers
 for i in range(lenght):
 	while rand_int in arr:
-		rand_int = randint(0, max_int)
+		rand_int = randint(min_int, max_int)
 	arr.append(rand_int)
+
+
+#Printing the array
+print("Array before sort - ", arr)
 
 #Sorting the array
 for i in range(lenght):
@@ -25,8 +30,5 @@ for i in range(lenght):
 	index_min = arr.index(e_min)
 	sorted_arr.append(arr.pop(index_min))
 
-#Printing the arrays
-print("Array before sort - "arr)
-print("Array after sort - "sorted_arr)
-
-
+#Printing the array
+print("Array after sort - ", sorted_arr)
